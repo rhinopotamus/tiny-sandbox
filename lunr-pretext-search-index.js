4b132ab733c10fa7af16a60dfd608c58c02bbfa9
Spencer Bagley <orangefish@gmail.com>
2025-11-04 13:13:28 -0700

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -10,18 +10,18 @@ var ptx_lunr_docs = [
   "body": "  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-proteus-5-1",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-proteus-5-1.html",
   "type": "Section",
   "number": "1.1",
-  "title": "A bunch of PROTEUS drafts",
-  "body": " A bunch of PROTEUS drafts  Drafts of questions for PROTEUS chapter 5 stuff.   PROTEUS exercises (for Section 5.1)    Drag each function on the left to the graph of its antiderivative on the right.          f(x)=3*x^2 + 2*x               f(x)=x^3 + x^2                 f(x)=sin(x)+ 1               f(x)=-cos(x)+x                 f(x)=2*x\/(x^2+1)               f(x)=ln(x^2+1)                 f(x)=-1  g(x)=1                f(x)=fabs(x)            decoy 1    decoy 2       PROTEUS exercises (for Section 5.1)    Drag each function on the left to the graph of its antiderivative on the right.         f(x)=3*x^2 + 2*x               f(x)=x^3 + x^2               f(x)=sin(x)+ 1               f(x)=-cos(x)+x               f(x)=2*x\/(x^2+1)               f(x)=ln(x^2+1)               f(x)=-1  g(x)=1                f(x)=fabs(x)              PROTEUS exercises (for Section 5.2)    Consider the function defined by the rule . In the figures below, is the function graphed in blue. is found by computing the area shaded in gray in the figure on the left, and is found by computing the area shaded in green in the figure on the right.     f(t)=t^3\/3 - 2*t^2 + 3*t +2       P    f(t)          f(t)=t^3\/3 - 2*t^2 + 3*t +2       Q    f(t)    3+\\Delta x            Label the coordinates of point and of point .      As long as is relatively small, is well approximated by a rectangle. Draw such a rectangle on the figure above, and label its height and its width.      Since is approximately a rectangle, is approximately the product of the height and the width. Use your labels from the previous part to write an approximation for .       What is ?        Consider the piecewise linear function defined by this graph on the interval :    f(x)=x  g(x)=4-x  h(x)=x-8          Let be the function defined by the rule .     Reasoning from the graph, fill in the values of in the table below.                                 yay good job         yay good job         yay good job         yay good job         yay good job         yay good job         yay good job         yay good job         Name two trends or patterns that you observe in the values of that you found in part (a).       On the interval , do you expect the graph of to be constant, linear, quadratic, or something else? Why?         PROTEUS exercises (for Section 5.3)   Each of the following expressions is the result of the Chain Rule being applied to a composite function. In other words, it is the right-hand side of the Chain Rule: , OR, Eyerusalem requested to see what it looks like, version 2: where is an antiderivative of , so that .  In each of the problems below, your job is to identify the functions , , and , recover the antiderivative , and then calculate the original composite function .      Drag the cards from the left column into the correct labels in the right column.                             Drag the cards from the left column into the correct labels in the right column.                             Drag the cards from the left column into the correct labels in the right column.                           "
+  "title": "PROTEUS drafts 5.1",
+  "body": " PROTEUS drafts 5.1  Drafts of PROTEUS 5.1.   PROTEUS exercises (for Section 5.1)    Drag each function on the left to the graph of its antiderivative on the right.          f(x)=3*x^2 + 2*x               f(x)=x^3 + x^2                 f(x)=sin(x)+ 1               f(x)=-cos(x)+x                 f(x)=2*x\/(x^2+1)               f(x)=ln(x^2+1)                 f(x)=-1  g(x)=1                f(x)=fabs(x)            decoy 1    decoy 2       PROTEUS exercises (for Section 5.1)    Drag each function on the left to the graph of its antiderivative on the right.         f(x)=3*x^2 + 2*x               f(x)=x^3 + x^2               f(x)=sin(x)+ 1               f(x)=-cos(x)+x               f(x)=2*x\/(x^2+1)               f(x)=ln(x^2+1)               f(x)=-1  g(x)=1                f(x)=fabs(x)             "
 },
 {
   "id": "proteus-5-1-matching-graphs",
   "level": "2",
-  "url": "sec-section-name.html#proteus-5-1-matching-graphs",
+  "url": "sec-proteus-5-1.html#proteus-5-1-matching-graphs",
   "type": "Exercise",
   "number": "1.1.1",
   "title": "",
@@ -30,56 +30,83 @@ var ptx_lunr_docs = [
 {
   "id": "proteus-5-1-matching-graphs-2",
   "level": "2",
-  "url": "sec-section-name.html#proteus-5-1-matching-graphs-2",
+  "url": "sec-proteus-5-1.html#proteus-5-1-matching-graphs-2",
   "type": "Exercise",
   "number": "1.1.1",
   "title": "",
   "body": "  Drag each function on the left to the graph of its antiderivative on the right.         f(x)=3*x^2 + 2*x               f(x)=x^3 + x^2               f(x)=sin(x)+ 1               f(x)=-cos(x)+x               f(x)=2*x\/(x^2+1)               f(x)=ln(x^2+1)               f(x)=-1  g(x)=1                f(x)=fabs(x)           "
 },
 {
+  "id": "sec-proteus-5-2",
+  "level": "1",
+  "url": "sec-proteus-5-2.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "PROTEUS drafts 5.2",
+  "body": " PROTEUS drafts 5.2  Drafts of PROTEUS 5.2.   PROTEUS exercises (for Section 5.2)    Consider the function defined by the rule . In the figures below, is the function graphed in blue. is found by computing the area shaded in gray in the figure on the left, and is found by computing the area shaded in green in the figure on the right.     f(t)=t^3\/3 - 2*t^2 + 3*t +2       P    f(t)          f(t)=t^3\/3 - 2*t^2 + 3*t +2       Q    f(t)    3+\\Delta x            Label the coordinates of point and of point .      As long as is relatively small, is well approximated by a rectangle. Draw such a rectangle on the figure above, and label its height and its width.      Since is approximately a rectangle, is approximately the product of the height and the width. Use your labels from the previous part to write an approximation for .       What is ?        Consider the piecewise linear function defined by this graph on the interval :    f(x)=x  g(x)=4-x  h(x)=x-8          Let be the function defined by the rule .     Reasoning from the graph, fill in the values of in the table below.                                 yay good job         yay good job         yay good job         yay good job         yay good job         yay good job         yay good job         yay good job         Name two trends or patterns that you observe in the values of that you found in part (a).       On the interval , do you expect the graph of to be constant, linear, quadratic, or something else? Why?        "
+},
+{
   "id": "proteus-5-2-spencer",
   "level": "2",
-  "url": "sec-section-name.html#proteus-5-2-spencer",
+  "url": "sec-proteus-5-2.html#proteus-5-2-spencer",
   "type": "Exercise",
-  "number": "1.1.1",
+  "number": "1.2.1",
   "title": "",
   "body": "  Consider the function defined by the rule . In the figures below, is the function graphed in blue. is found by computing the area shaded in gray in the figure on the left, and is found by computing the area shaded in green in the figure on the right.     f(t)=t^3\/3 - 2*t^2 + 3*t +2       P    f(t)          f(t)=t^3\/3 - 2*t^2 + 3*t +2       Q    f(t)    3+\\Delta x            Label the coordinates of point and of point .      As long as is relatively small, is well approximated by a rectangle. Draw such a rectangle on the figure above, and label its height and its width.      Since is approximately a rectangle, is approximately the product of the height and the width. Use your labels from the previous part to write an approximation for .       What is ?     "
 },
 {
   "id": "proteus-5-2-matt",
   "level": "2",
-  "url": "sec-section-name.html#proteus-5-2-matt",
+  "url": "sec-proteus-5-2.html#proteus-5-2-matt",
   "type": "Exercise",
-  "number": "1.1.2",
+  "number": "1.2.2",
   "title": "",
   "body": "  Consider the piecewise linear function defined by this graph on the interval :    f(x)=x  g(x)=4-x  h(x)=x-8          Let be the function defined by the rule .     Reasoning from the graph, fill in the values of in the table below.                                 yay good job         yay good job         yay good job         yay good job         yay good job         yay good job         yay good job         yay good job         Name two trends or patterns that you observe in the values of that you found in part (a).       On the interval , do you expect the graph of to be constant, linear, quadratic, or something else? Why?      "
 },
 {
+  "id": "sec-proteus-5-3",
+  "level": "1",
+  "url": "sec-proteus-5-3.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "PROTEUS drafts 5.3",
+  "body": " PROTEUS drafts 5.3  Drafts of PROTEUS 5.3.   PROTEUS exercises (for Section 5.3)   Each of the following expressions is the result of the Chain Rule being applied to a composite function. In other words, it is the right-hand side of the Chain Rule: , where is an antiderivative of , so that .  In each of the problems below, your job is to identify the functions , , and .       Drag the cards from the left column into the correct labels in the right column.      Inner function     Derivative of inner function     Outer function         Drag the cards from the left column into the correct labels in the right column.      Inner function     Derivative of inner function     Outer function         Drag the cards from the left column into the correct labels in the right column.      Inner function     Derivative of inner function     Outer function         Drag the cards from the left column into the correct labels in the right column.      Inner function     Almost the derivative of inner function     Outer function       "
+},
+{
   "id": "proteus-5-3-matching-1",
   "level": "2",
-  "url": "sec-section-name.html#proteus-5-3-matching-1",
+  "url": "sec-proteus-5-3.html#proteus-5-3-matching-1",
   "type": "Exercise",
-  "number": "1.1.1",
+  "number": "1.3.1",
   "title": "",
-  "body": "   Drag the cards from the left column into the correct labels in the right column.                         "
+  "body": "   Drag the cards from the left column into the correct labels in the right column.      Inner function     Derivative of inner function     Outer function     "
 },
 {
   "id": "proteus-5-3-matching-2",
   "level": "2",
-  "url": "sec-section-name.html#proteus-5-3-matching-2",
+  "url": "sec-proteus-5-3.html#proteus-5-3-matching-2",
   "type": "Exercise",
-  "number": "1.1.2",
+  "number": "1.3.2",
   "title": "",
-  "body": "   Drag the cards from the left column into the correct labels in the right column.                         "
+  "body": "   Drag the cards from the left column into the correct labels in the right column.      Inner function     Derivative of inner function     Outer function     "
 },
 {
   "id": "proteus-5-3-matching-3",
   "level": "2",
-  "url": "sec-section-name.html#proteus-5-3-matching-3",
+  "url": "sec-proteus-5-3.html#proteus-5-3-matching-3",
   "type": "Exercise",
-  "number": "1.1.3",
+  "number": "1.3.3",
   "title": "",
-  "body": "   Drag the cards from the left column into the correct labels in the right column.                         "
+  "body": "   Drag the cards from the left column into the correct labels in the right column.      Inner function     Derivative of inner function     Outer function     "
+},
+{
+  "id": "proteus-5-3-matching-4",
+  "level": "2",
+  "url": "sec-proteus-5-3.html#proteus-5-3-matching-4",
+  "type": "Exercise",
+  "number": "1.3.4",
+  "title": "",
+  "body": "   Drag the cards from the left column into the correct labels in the right column.      Inner function     Almost the derivative of inner function     Outer function     "
 },
 {
   "id": "backmatter-2",
